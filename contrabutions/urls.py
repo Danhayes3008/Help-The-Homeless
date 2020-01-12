47d0f1eae3062cdf77e3e2969424d68491c6486b
danhayes <danhayes3008@outlook.com>
2020-01-12 18:22:51 +0000

    adjusted the import

--- contrabutions/urls.py
+++ contrabutions/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
-from .views import projects
+from .views import contrabution
 
 urlpatterns = [
-    url(r'^$', projects, name="donation"),
+    url(r'^$', contrabution, name="donation"),
 ]
