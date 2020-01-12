e27557c4c63f1d4a4e1a7d305ff28edde2ec221e
danhayes <danhayes3008@outlook.com>
2020-01-12 15:20:39 +0000

    added the url import for my contact app

--- charity/urls.py
+++ charity/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from homepage.views import index
 from accounts import urls as urls_accounts
+from contact import urls as urls_contact
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include(urls_accounts))
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^contact/', include(urls_contact)),
 ]
