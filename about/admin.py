412b89bd3a4bece5641af675b6f01175a0aa0327
danhayes <danhayes3008@outlook.com>
2020-01-12 15:39:13 +0000

    added the models to the admin.py so that i can add data to the database in the admin pannel

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import total_homeless, yearly_homeless
 
-# Register your models here.
+admin.site.register(total_homeless)
+admin.site.register(yearly_homeless)
