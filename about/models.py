bd9d6e1ce9627ec8acc564a7cbe379ef255af493
danhayes <danhayes3008@outlook.com>
2020-01-12 15:37:09 +0000

    added the models from my first attempt to the project

--- about/models.py
+++ about/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class total_homeless(models.Model):
+    name = models.CharField(max_length=250, default="")
+    totalHomeless = models.DecimalField(max_digits=9, decimal_places=0)
+    totalPopulation = models.DecimalField(max_digits=9, decimal_places=0)
+    
+    def __str__(self):
+        return self.name
+
+class yearly_homeless(models.Model):
+    name = models.CharField(max_length=250, default="")
+    year = models.DecimalField(max_digits=4, decimal_places=0)
+    total = models.DecimalField(max_digits=9, decimal_places=0)
+    
+    def __str__(self):
+        return self.name
