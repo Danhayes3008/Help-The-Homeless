dcb94bffa3849f5be5fd3a5872cb85d343c9d941
danhayes <danhayes3008@outlook.com>
2020-01-12 18:43:21 +0000

    added the cart app, transfered and edited the code from my failed projects donate app

---
+++ cart/contexts.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404
+from contrabutions.models import Donations
+
+def housing_cart_contents(request):
+    """
+    Ensures that the cart contents are available when rendering every page
+    """
+    
+    cart = request.session.get('cart', {})
+    
+    cart_items = []
+    total = 0
+    project_count = 0
+    for id, amount in cart.items():
+        donations = get_object_or_404(Donations, pk=id)
+        total += amount * donations.donation
+        project_count += amount
+        cart_items.append({'id':id, 'amount':amount, 'donations':donations})
+        
+    return {'cart_items': cart_items, 'total':total, 'project_count':project_count}
---
+++ cart/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect, reverse
+from contrabutions.models import Donations
+
+# Create your views here.
+def view_donate(request):
+    """A View that renders the cart contents page"""
+    return render(request, "cart.html")
+
+
+def amount_to_donate(request, id):
+    """Add a quantity of the specified product to the cart"""
+    amount = int(request.POST.get('amount'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + amount      
+    else:
+        cart[id] = cart.get(id, amount) 
+
+    request.session['cart'] = cart
+    return redirect(reverse('donation'))
+
+
+def adjust_donation(request, id):
+    """
+    Adjust the quantity of the specified donation to the specified
+    amount
+    """
+    print(request.POST)
+    amount = int(request.POST.get('amount'))
+    cart = request.session.get('cart', {})
+
+    if amount > 0:
+        cart[id] = amount
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_donate'))
