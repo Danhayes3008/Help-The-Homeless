f2ab8677d708d21b7c32095c03d98e2695da1aa1
danhayes <danhayes3008@outlook.com>
2020-01-12 18:00:35 +0000

    updated the view for contrabution

--- contrabutions/views.py
+++ contrabutions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def contrabution(request):
-    return render(request, 'contrabution.html')
+    donations = Donations.objects.all()
+    return render(request, 'contrabution.html', {"donations": donations})
