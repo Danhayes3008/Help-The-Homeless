f3ea44967ab4028d2f9b69ed14ad1966c7c1fa3b
danhayes <danhayes3008@outlook.com>
2020-01-12 18:06:28 +0000

    added the Donations model

--- contrabutions/models.py
+++ contrabutions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Donations(models.Model):
+    name = models.CharField(max_length=254, default='')
+    donation = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
+    minDonation = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
+    
+    class Meta:
+        verbose_name_plural = "Donations"
+        
+    def __str__(self):
+        return self.name
