b27316ca3ea25dbb0b3b9a447d5c8c88798b7ddd
danhayes <danhayes3008@outlook.com>
2020-01-13 14:06:02 +0000

    added the model for payments

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from contrabutions.models import Donations
 # Create your models here.
+
+class donate(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    streetadress1 = models.CharField(max_length=40, blank=False)
+    streetaddress2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+    
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        
+class OrderLineItem(models.Model):
+    donate = models.ForeignKey(Donate, null=False)
+    donation = models.ForeignKey(Donations, null=False)
+    quantity = models.IntegerField(blank=False)
+    
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.donation.name, self.donation.price)
