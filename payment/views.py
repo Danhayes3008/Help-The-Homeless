b26b10f457e64aa6a8d52949516f467cb3fa11d0
danhayes <danhayes3008@outlook.com>
2020-01-13 14:40:03 +0000

    started work on the def payment

--- payment/views.py
+++ payment/views.py
@@ -9,3 +9,14 @@ from contrabutions.models import Donations
 import stripe
 
 stripe.api_key = settings.STRIPE_KEY
+
+@login_required()
+def payment (request):
+    if request.method=="POST":
+        donate_form = DonateForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if donate_form.is_valid() and payment_form.is_valid():
+            donate = donate_form.save(commit=False)
+            donate.date = timezone.now()
+            donate.save()
