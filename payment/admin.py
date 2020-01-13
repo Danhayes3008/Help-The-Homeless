cef892a66ab654173bdae9fd2c0e789cb51e2263
danhayes <danhayes3008@outlook.com>
2020-01-13 16:26:07 +0000

    added the classes that allow the admin to see the models in this app in the admin pannel

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import donate, DonateLineItem
 # Register your models here.
+
+class DonateLineAdminInline(admin.TabularInline):
+    model = DonateLineItem
+    
+class DonateAdmin(admin.ModelAdmin):
+    inlines = (DonateLineAdminInline, )
+    
+admin.site.register(donate, DonateAdmin)
