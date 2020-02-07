dd7a05c2e1aaff5916a78314b1236822b24d59cc
danhayes <danhayes3008@outlook.com>
2020-02-07 13:50:24 +0000

    removed the first name and last name from the register form as they wont be needed

--- accounts/forms.py
+++ accounts/forms.py
@@ -29,7 +29,7 @@ class RegistrationForm(UserCreationForm):
         widget=forms.PasswordInput)    
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         
     def clean_email(self):
         email = self.cleaned_data.get('email')
