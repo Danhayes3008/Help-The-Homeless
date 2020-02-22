b529ce479633a28dd0c16674c1b759928052dc48
danhayes <danhayes3008@outlook.com>
2020-02-22 22:44:14 +0000

    adjusted the form to use the new model name

--- payment/forms.py
+++ payment/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from.models import donate
+from.models import Details
 
 class MakePaymentForm(forms.Form):
     MONTH_CHOICE = [(i, i) for i in range(1, 12)]
@@ -11,7 +11,7 @@ class MakePaymentForm(forms.Form):
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICE, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
     
-class DonateForm(forms.ModelForm):
+class DetailsForm(forms.ModelForm):
     class Meta:
-        model = donate
+        model = Details
         fields = ('street_address_1', 'street_address_2', 'town_or_city','county', 'postcode', 'country')
