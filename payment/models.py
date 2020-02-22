e7521b07963a39aca3fe16e23d40295d3730fa14
danhayes <danhayes3008@outlook.com>
2020-02-22 22:43:26 +0000

    restructured the donate model and renamed it. renamed a field in the donatelineitem model and added a related_name to it

--- payment/models.py
+++ payment/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 from contrabutions.models import Donations
+from django.contrib.auth.models import User
+
 # Create your models here.
 
-class donate(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
+#This model will be used to hold all the information needed to submit donations, it will also be used to help display the users donation history
+class Details(models.Model):
+    name = models.CharField(max_length=50, blank=False)
     street_address_1 = models.CharField(max_length=40, blank=False)
     street_address_2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
     date = models.DateField()
     
     def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        return "{0}-{1}-{2}".format(self.id, self.date, self.name)
         
 class DonateLineItem(models.Model):
-    donate = models.ForeignKey(donate, null=False)
+    details = models.ForeignKey(Details, related_name="details")
     donations = models.ForeignKey(Donations, null=False)
     amount = models.IntegerField(blank=False)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.amount, self.donate.full_name, self.donations.name, self.donations.donation)
+        return "{0} {1} @ {2}".format(self.amount, self.details.name, self.donations.name, self.donations.donation)
