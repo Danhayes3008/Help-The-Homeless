8721a881988d1b28878369563a4f1a39cc7e64ff
danhayes <danhayes3008@outlook.com>
2020-02-22 17:23:24 +0000

    added a url for the change password page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from . import views
 from accounts.views import login, logout, registration, profile, update_profile, delete_user
 from accounts import url_reset
 
@@ -8,5 +9,6 @@ urlpatterns = [
     url(r'^profile/', profile, name='profile'),
     url(r'^register/', registration, name='register'),
     url(r'^update/', update_profile, name='update_profile'),
+    url(r'^change_password/', views.change_password, name='change_password'),
     url(r'^password-reset/', include(url_reset)),
 ]
