0699a4f16dc31ff6078414154a007a1f50dd4ad3
danhayes <danhayes3008@outlook.com>
2020-02-23 17:52:57 +0000

    attempting to fix the problem with the test were it shows the page as not loaded

--- contrabutions/test_views.py
+++ contrabutions/test_views.py
@@ -5,6 +5,6 @@ from .views import contrabution
 class TestViews(TestCase):
 
     def test_get_contrabution_page(self):
-        page = self.client.get("contrabution/")
+        page = self.client.get("/contrabution")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "contrabution.html")
