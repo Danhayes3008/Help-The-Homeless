00c819d77a7dc20cc114b2bd6dd2de9d29f5a353
danhayes <danhayes3008@outlook.com>
2020-02-23 17:48:11 +0000

    created the test that should come back as a pass when contrabution page is loaded

---
+++ contrabutions/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .views import contrabution
+
+
+class TestViews(TestCase):
+
+    def test_get_contrabution_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "contrabution.html")
