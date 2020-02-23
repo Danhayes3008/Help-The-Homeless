338617ed1c1c960c55402a65b7acc227d4348f87
danhayes <danhayes3008@outlook.com>
2020-02-23 17:29:37 +0000

    hoping the new client.get works

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -5,6 +5,6 @@ from .views import profile
 class TestViews(TestCase):
 
     def test_get_profile_page(self):
-        page = self.client.get("/accounts/profile/")
+        page = self.client.get("accounts/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "profile.html")
+        self.assertTemplateUsed(page, "accounts/profile.html")
