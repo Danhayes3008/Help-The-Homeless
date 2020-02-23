7cb5b8d8ae9831cf506d6b7f8a94915384b528ae
danhayes <danhayes3008@outlook.com>
2020-02-23 16:57:10 +0000

    created the test to see if the profile page runs

---
+++ accounts/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .views import profile
+
+
+class TestViews(TestCase):
+
+    def test_get_profile_page(self):
+        page = self.client.get("/profile")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "profile.html")
