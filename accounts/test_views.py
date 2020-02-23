28fa80450dce7dda9e7c29fde909ce98e2ac1f4e
danhayes <danhayes3008@outlook.com>
2020-02-23 18:07:28 +0000

    updating the test now i know how to do it

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from .views import profile, login
 
 
-# class TestViews(TestCase):
+class TestViews(TestCase):
 
-#     def test_get_login_page(self):
-#         page = self.client.get("/")
-#         self.assertEqual(page.status_code, 200)
-#         self.assertTemplateUsed(page, "login.html")
+    def test_get_login_page(self):
+        page = self.client.get("/login/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "login.html")
