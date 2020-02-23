d919eef856517f1c6d3ca1bf6bc5beef2e4a0bb5
danhayes <danhayes3008@outlook.com>
2020-02-23 13:47:09 +0000

    updated the user field to include a related name so that the model is linked to the profile model

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     nationality = models.CharField(max_length=100, blank=True)
     birthday = models.DateField(auto_now=False, null=True, blank=True)
     image = models.ImageField(upload_to='images', null=True, blank=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
         return self.user.username
