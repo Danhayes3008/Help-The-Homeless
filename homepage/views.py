8c0f912799a038c90dafb3baed7d873523ffeb35
danhayes <danhayes3008@outlook.com>
2020-02-23 01:50:31 +0000

    fixed the homepage view.py so it works with the new coding

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,5 @@ from contrabutions.models import Donation
 
 # Create your views here.
 def index(request):
-    donation = Donation.objects.all()
-    return render(request, 'index.html', {"donation": donation})
+    donations = Donation.objects.all()
+    return render(request, 'index.html', {"donations": donations})
