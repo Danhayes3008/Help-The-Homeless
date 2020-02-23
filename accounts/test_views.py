12bb1596c251d2c47501bba27d3912ffcbeca4c3
danhayes <danhayes3008@outlook.com>
2020-02-23 19:35:38 +0000

    realised i was trying to add a user to the login page test when it don't need one so created a new class to deal with the profile page

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -1,29 +1,23 @@
 from django.test import TestCase
-from django.contrib.auth import SESSION_KEY
 from .views import profile, login, update_profile
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class TestViews(TestCase):
 
     def test_get_login_page(self):
-        def setUp(self):
-            self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-            User.objects.create_user(**self.credentials)
-        
-        page = self.client.get("/accounts/login/", self.credentials, follow=True)
+        page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
-        response = self.client.post('/login/', **self.credentials)
-        self.assertTrue(response.context['user'].is_active)
+        self.assertTemplateUsed(page, "login.html")
     
-    def test_get_profile_page(self):
-        page = self.client.get("/accounts/profile/", self.credentials, follow=True)
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "profile.html")
-        
-    # def test_get_update_page(self):
-    #     page = self.client.get("/accounts/update/")
-    #     self.assertEqual(page.status_code, 200)
-    #     self.assertTemplateUsed(page, "update.html")
+    
+class TestProfileViews(TestCase):
+    self.user = User.objects.create(username='admin', password='pass@123', email='admin@admin.com')
+    self.client = profile() # May be you have missed this line
+
+    def test_history(self):
+        self.client.login(username=self.user.username, password='pass@123')
+        # get_history function having login_required decorator
+        response = self.client.post(reverse('profile'), {'user_id': self.user.id})
+        self.assertEqual(response.status_code, 200)
