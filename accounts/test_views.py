e726ab9657078fe66e016f2576c1846dcd99fb17
danhayes <danhayes3008@outlook.com>
2020-02-23 18:09:40 +0000

    trying to resolve the error thats stopping the test form passing

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -5,6 +5,6 @@ from .views import profile, login
 class TestViews(TestCase):
 
     def test_get_login_page(self):
-        page = self.client.get("/login/")
+        page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "login.html")
