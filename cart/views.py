b8a0521b9da6f6c52f39e936e23045257a01a925
danhayes <danhayes3008@outlook.com>
2020-03-06 13:00:35 +0000

    added comments to each view so the reader knows what they are for

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from contrabutions.models import Donation
 
-# Create your views here.
+# This view renders the cart page.
 def view_donate(request):
     """A View that renders the cart contents page"""
     return render(request, "cart.html")
 
-
+# This view manages the part that adds the stuff to the cart
 def amount_to_donate(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
@@ -20,7 +20,7 @@ def amount_to_donate(request, id):
     request.session['cart'] = cart
     return redirect(reverse('donation'))
 
-
+# This part allows the user to adjust the cart before going to the payment page
 def adjust_donation(request, id):
     """
     Adjust the quantity of the specified donation to the specified
