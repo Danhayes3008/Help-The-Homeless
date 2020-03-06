5da5936a72f8522e960f28b9d6785f434ca3cab9
danhayes <danhayes3008@outlook.com>
2020-03-06 12:44:40 +0000

    added comments explaining what the content does

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
-# Register your models here.
+# This registers the profile model.
 admin.site.register(Profile)
+# This line of code renames the admin panels site header
 admin.site.site_header = 'Help The Homeless Administration'
