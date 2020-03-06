ba1aa6fc1718b1042ced85681b125a8a7d39f1a2
danhayes <danhayes3008@outlook.com>
2020-03-06 12:45:45 +0000

    added comments to explain the model so the reader knows that  everything does

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
+# This model creates the profile table 
 class Profile(models.Model):
     name = models.CharField(max_length=250, blank=True)
     gender = models.CharField(max_length=6, blank=True)
@@ -14,7 +14,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+    # this def is an attempt to resize images when the profile is updated to resolve the issue where they look either stretched or squashed
     def save(self, force_insert=False, force_update=False, *args, **kwargs,):        
         super(Profile, self).save(force_insert, force_update, *args, **kwargs)
         if self.id is not None:
@@ -23,7 +23,7 @@ class Profile(models.Model):
                 images = images.open(self.Profile.path)
                 images = images.resize((225, 225), images.ANTIALIAS)
                 images.save(self.logo.path)
-    
+# This section creates a profile for the user when an account is made
 @receiver(post_save, sender=User)
 def create_or_update_user_profile (sender, instance, created, **kwargs):
     if created:
