97232bf8cc9eaa4664b85fca63c94bd552930c74
danhayes <danhayes3008@outlook.com>
2020-03-06 13:36:44 +0000

    added a comment avout the view in this file

--- contrabutions/views.py
+++ contrabutions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Donation
-
+# This view renders the page and searches for all items in the Donation table
 def contribution(request):
     donations = Donation.objects.all()
     print(donations)
