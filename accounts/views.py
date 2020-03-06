f9d5b21e8bc2104c0b4c8a7b43fed9881ed39bc1
danhayes <danhayes3008@outlook.com>
2020-03-06 10:17:10 +0000

    adjusted the registration section

--- accounts/views.py
+++ accounts/views.py
@@ -56,8 +56,8 @@ def registration(request):
                 auth.login(user=user, request=request)
                 messages.success(request, "Welcome to Help the Homless")
                 return redirect(reverse('index'))
-        else:
-            messages.error(request, "Something went wrong, please try again")
+            else:
+                messages.error(request, "Something went wrong, please try again")
     else:
         registration_form = RegistrationForm()
     return render(request, 'registration.html',
