81e69afd7b5b2e3dd50c68f3fdc0c122da41c6a1
danhayes <danhayes3008@outlook.com>
2020-03-06 12:04:22 +0000

    added a comment stating what the test is for

--- about/test_views.py
+++ about/test_views.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 from .views import about
 
-
+# This test shows is the page loads or not
 class TestViews(TestCase):
-
     def test_get_about_page(self):
         page = self.client.get("/about_us/about/")
         self.assertEqual(page.status_code, 200)
