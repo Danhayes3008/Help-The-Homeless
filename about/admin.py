4c830705e9dfb191e537546b5d0b2a27ded23ede
danhayes <danhayes3008@outlook.com>
2020-03-06 12:03:45 +0000

    add a comment explaining what the two lines of code here do

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import total_homeless, yearly_homeless
-
+# These lines of code allow us to use the models in the admin pannel
 admin.site.register(total_homeless)
 admin.site.register(yearly_homeless)
