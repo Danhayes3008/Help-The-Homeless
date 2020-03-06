f3e08620a3392955c1dfcee911fd73adb6820061
danhayes <danhayes3008@outlook.com>
2020-03-06 13:00:47 +0000

    added a comment about the urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .views import view_donate, amount_to_donate, adjust_donation
-
+# These are the urls that help manage the donating and rendering of the cart page
 urlpatterns = [
     url(r'^$', view_donate, name='view_donate'),
     url(r'^add/(?P<id>\d+)', amount_to_donate, name='amount_to_donate'),
