a4802326ce1bfefe09ceac9cc125c38553c2b7fd
danhayes <danhayes3008@outlook.com>
2020-03-06 13:37:47 +0000

    added a comment about what the url patterns is for

--- contrabutions/urls.py
+++ contrabutions/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from .views import contribution
-
+# This is the url for the contribution page
 urlpatterns = [
     url(r'^$', contribution, name="donation"),
 ]
