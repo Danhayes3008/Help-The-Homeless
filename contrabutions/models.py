b18b505dc968969b85243d752af36afd2ee72943
danhayes <danhayes3008@outlook.com>
2020-03-06 13:37:34 +0000

    add a comment about what this model is for

--- contrabutions/models.py
+++ contrabutions/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# This is the model that allows us to create an item for managing the donations with.
 class Donation(models.Model):
     name = models.CharField(max_length=254, default='')
     donation_amount = models.DecimalField(max_digits=9, decimal_places=2, default='1.00')
-    
+    # This class sets the name of the table in the admin pannel 
     class Meta:
         verbose_name_plural = "Donation"
         
