cd686ff7d382e0125f16da02434b882db9c77b45
danhayes <danhayes3008@outlook.com>
2020-03-06 12:46:31 +0000

    added a comment explaining what the url_reset is for

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
-
+# these urlpatterns deal with the reset password feature so that the user can have a reset password link sent to them
 urlpatterns = [
     url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
