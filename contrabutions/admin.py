7afa12afad1847e1b6eb09ff4ee7ecf1ba8fdbac
danhayes <danhayes3008@outlook.com>
2020-03-06 13:37:20 +0000

    added a comment about what the register does

--- contrabutions/admin.py
+++ contrabutions/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Donation
-
+# This line of code registers the donation model so we can access it in the admin pannel
 admin.site.register(Donation)
