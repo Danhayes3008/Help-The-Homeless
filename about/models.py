f1d1c06eb62cc0e23660539c36c92ec8775214e7
danhayes <danhayes3008@outlook.com>
2020-03-06 12:04:04 +0000

    added comments explaining what the models are for

--- about/models.py
+++ about/models.py
@@ -1,5 +1,6 @@
+# These models are not needed at all but i have left them in so that the admin could record the information that would be needed for the charts 
 from django.db import models
-
+# This model is used to store what the total homeless is
 class total_homeless(models.Model):
     name = models.CharField(max_length=250, default="")
     totalHomeless = models.DecimalField(max_digits=9, decimal_places=0)
@@ -10,7 +11,7 @@ class total_homeless(models.Model):
     
     def __str__(self):
         return self.name
-
+# This model is used to show the yearly homeless totals
 class yearly_homeless(models.Model):
     name = models.CharField(max_length=250, default="")
     year = models.DecimalField(max_digits=4, decimal_places=0)
