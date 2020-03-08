e22a0bb8f7d2c37f881f515805bac41b38ddae2d
danhayes <danhayes3008@outlook.com>
2020-03-08 14:40:09 +0000

    noted what the urlpatterns in this file are for

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .views import payment
-
+# This url pattern holds the url for the payment page
 urlpatterns = [
     url(r'^$', payment, name='payment'),
     ]
