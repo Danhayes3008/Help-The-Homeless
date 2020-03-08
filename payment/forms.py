9b70f403bf023daf9b05947acd62186c5d5935fc
danhayes <danhayes3008@outlook.com>
2020-03-08 14:39:19 +0000

    added comments about the models in this file

--- payment/forms.py
+++ payment/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from.models import Details
-
+# This form tells stripe what card to charge
 class MakePaymentForm(forms.Form):
     MONTH_CHOICE = [(i, i) for i in range(1, 12)]
     YEAR_CHOICE = [(i, i) for i in range(2017, 2036)]
@@ -10,7 +10,8 @@ class MakePaymentForm(forms.Form):
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICE, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICE, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
-    
+
+# This form deals with address where the users receives the gift from there donation  
 class DetailsForm(forms.ModelForm):
     class Meta:
         model = Details
