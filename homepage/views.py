df9e20099cf86af968eacebf12e6700583c127e4
danhayes <danhayes3008@outlook.com>
2020-03-08 13:24:54 +0000

    added a comment on the def for that renders the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from contrabutions.models import Donation
 
-# Create your views here.
+# This view renders the home page and witht the donations bit it allows us to donate to the charity.
 def index(request):
     donations = Donation.objects.all()
     return render(request, 'index.html', {"donations": donations})
