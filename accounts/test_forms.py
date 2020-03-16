48a7c271b20bdfc1cb032dff8846c2062efe61a9
danhayes <danhayes3008@outlook.com>
2020-03-16 12:37:14 +0000

    added the setup def to try and get my test to work

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -2,9 +2,16 @@ from django.test import TestCase
 from django.test import SimpleTestCase
 from .forms import UserForm, LoginForm
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class TestLoginForm(SimpleTestCase):
-    self.user = User.objects.create(username='andy', password='pass@123', email='adndy@gmail.com')
+    def setUp(self):
+        self.register_url=reverse('register')
+        self.user ={
+            'username': 'andy',
+             'password': 'pass@123',
+             'email': 'adndy@gmail.com'
+        }
     
     def test_the_login_form(self):
         form = LoginForm(data={
