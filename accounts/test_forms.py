45545fc5ae8e2be04f8b15ede57254da29f7ac28
danhayes <danhayes3008@outlook.com>
2020-03-16 12:47:11 +0000

    trying to test the registration form

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
-from .forms import UserForm, LoginForm
+from .forms import UserForm, LoginForm, RegistrationForm
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -17,5 +17,15 @@ class TestLoginForm(SimpleTestCase):
         form = LoginForm(data={
             'username': 'andy',
             'password': 'pass@123'
+        })
+        self.assertTrue(form.is_valid())
+
+class TestRegistrationForm(SimpleTestCase):
+    def test_the_registration_form(self):
+        form = RegistrationForm({
+            'username': 'andy',
+            'email': 'test@gmail.com',
+            'password1': 'test1234',
+            'password2': 'test1234'
         })
         self.assertTrue(form.is_valid())
