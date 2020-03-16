be480ace4fa15e91fd1e9a9be6549b9e0300ca8a
danhayes <danhayes3008@outlook.com>
2020-03-16 16:41:52 +0000

    removed an empty line

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -10,8 +10,7 @@ class TestViews(TestCase):
     def test_get_login_page(self):
         page = self.client.get("/accounts/login/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "login.html")
-    
+        self.assertTemplateUsed(page, "login.html")    
     
 class BaseTest(TestCase):
     def setUp(self):
