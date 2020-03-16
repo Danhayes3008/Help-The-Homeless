6c09df53190b0bf5334295d926a8f097bb894b5f
danhayes <danhayes3008@outlook.com>
2020-03-16 12:07:59 +0000

    trying to get another test to work

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .views import profile, login, update_profile
 from django.contrib.auth.models import User
 from django.urls import reverse
+from accounts.models import Profile
 
 # this test checks if the login page loads or not
 class TestViews(TestCase):
@@ -12,12 +13,12 @@ class TestViews(TestCase):
         self.assertTemplateUsed(page, "login.html")
     
     
-# class TestProfileViews(TestCase):
-#     self.user = User.objects.create(username='admin', password='pass@123', email='admin@admin.com')
-#     self.client = profile() # May be you have missed this line
+class TestProfileViews(TestCase):
+    self.user = User.objects.create(username='andy', password='pass@123', email='adndy@gmail.com')
+    self.client = Profile()
 
-#     def test_history(self):
-#         self.client.login(username=self.user.username, password='pass@123')
-#         # get_history function having login_required decorator
-#         response = self.client.post(reverse('profile'), {'user_id': self.user.id})
-#         self.assertEqual(response.status_code, 200)
+    def test_history(self):
+        self.client.login(username=self.user.username, password='pass@123')
+        # get_history function having login_required decorator
+        response = self.client.post(reverse('profile'), {'user_id': self.user.id})
+        self.assertEqual(response.status_code, 200)
