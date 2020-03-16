968b56cace964ffcd0e61dc1715aca55caa2118b
danhayes <danhayes3008@outlook.com>
2020-03-16 12:27:53 +0000

    attempting a different way to test the form

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
 from .forms import UserForm, LoginForm
 from django.contrib.auth.models import User
 
-class TestLoginForm(TestCase):
+class TestLoginForm(SimpleTestCase):
     self.user = User.objects.create(username='andy', password='pass@123', email='adndy@gmail.com')
     
     def test_the_login_form(self):
-        form = LoginForm({'username': 'andy'}, {'password': 'pass@123'})
+        form = LoginForm(data={
+            'username': 'andy',
+            'password': 'pass@123'
+        })
         self.assertTrue(form.is_valid())
