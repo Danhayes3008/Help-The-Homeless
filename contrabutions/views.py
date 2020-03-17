6eed6327022a5c69188d1f84ce61b3f216568f04
danhayes <danhayes3008@outlook.com>
2020-03-17 15:30:16 +0000

    removed the print funtcion as it is not needed anymore

--- contrabutions/views.py
+++ contrabutions/views.py
@@ -3,5 +3,4 @@ from .models import Donation
 # This view renders the page and searches for all items in the Donation table
 def contribution(request):
     donations = Donation.objects.all()
-    print(donations)
     return render(request, 'contribution.html', {"donations": donations})
