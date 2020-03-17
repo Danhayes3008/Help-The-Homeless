c6288cec37100bcaab08845edd9d93cf86153d09
danhayes <danhayes3008@outlook.com>
2020-03-17 12:56:50 +0000

    removed the models test

--- accounts/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Profile
-
-class TestProfileModel(self):
-    def test_create_a_profile(self):
-        profile = Profile(name="testname")
-        profile.save()
-        self.assertEqual(profile.name, "testname")
